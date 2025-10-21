bdb4bfd80da6b32dd0f08e2353c6619e946468a0
Aman Silawat <Akram.khan.silawat.aks@gmail.com>
2025-10-21 20:42:43 +0530

    Project Tabs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,60 @@
 import React, { useState, useEffect } from 'react'
 import { FaAngleDoubleRight } from 'react-icons/fa'
+import data from './data'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+
+const url = data
 function App() {
-  return <h2>tabs project setup</h2>
+  const [loading, setLoading] = useState()
+  const [jobs, setJobs] = useState([url])
+  const [value, setValue] = useState(0)
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json(url);
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className='section loading'>
+        <h1>loading...</h1>
+      </section>
+    );
+  }
+  const { company, dates, duties, title } = url[value];
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>expierence</h2>
+        <div className='underline'></div>
+      </div>
+      <div className='jobs-center'>
+        {/* btn container */}
+        {/* job info */}
+        <article className='job-info'>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className='job-desc'>
+                <FaAngleDoubleRight
+                  className='job-icon'></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default App
---
+++ src/data.js
@@ -0,0 +1,34 @@
+export default [
+    {
+        id: "recAGJfiU4CeaVOHL",
+        order: 3,
+        title: 'Full Stack Web Developer',
+        dates: "December 2015 - Present",
+        duties: ["Paragraphs can contain many different kinds of information. A paragraph could contain a series of brief examples or a single long illustration of a general point.",
+            "It might describe a place, character, or process; narrate a series of events; compare or contrast two or more things; classify items into categories",
+            "or describe causes and effects. Regardless of the kind of information they contain, all paragraphs share certain characteristics. One of the most important of these is a topic sentence."
+        ],
+        company: "TOMMY"
+    },
+    {
+        id: "recIL6mJNWObonls",
+        order: 2,
+        title: 'Front-End Engineer',
+        dates: "May 2015 - December 2015",
+        duties: ["Although most paragraphs should have a topic sentence, there are a few situations when a paragraph might not need a topic sentence. For example, you might be able to omit a",
+            "topic sentence in a paragraph that narrates a series of events, if a paragraph continues developing an idea that you introduced (with a topic sentence) in the previous paragraph,",
+            "or if all the sentences and details in a paragraph clearly refer—perhaps indirectly—to a main point. The vast majority of your paragraphs, however, should have a topic sentence."
+        ],
+        company: "BIGDROP"
+    }, {
+        id: "recAGJfiU4CeaVOHL",
+        order: 3,
+        title: 'Full Stack Web Developer',
+        dates: "December 2015 - Present",
+        duties: ["Paragraphs can contain many different kinds of information. A paragraph could contain a series of brief examples or a single long illustration of a general point.",
+            "It might describe a place, character, or process; narrate a series of events; compare or contrast two or more things; classify items into categories",
+            "or describe causes and effects. Regardless of the kind of information they contain, all paragraphs share certain characteristics. One of the most important of these is a topic sentence."
+        ],
+        company: "CUKER"
+    },
+];
